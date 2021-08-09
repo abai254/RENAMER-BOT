9f82bea038c9f04fa030bbf2a7f8ed5f494c4b21
abai254 <84307584+abai254@users.noreply.github.com>
2021-08-09 20:05:07 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -9,15 +9,8 @@ import asyncio
 
 from sqlalchemy import Column, Integer, Boolean, String, ForeignKey, UniqueConstraint, func
 
-
-if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
-    from config import Config
-
-
 def start() -> scoped_session:
-    engine = create_engine(Config.DB_URI, client_encoding="utf8")
+    engine = create_engine(os.environ.get("DATABASE_URL", ""), client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
